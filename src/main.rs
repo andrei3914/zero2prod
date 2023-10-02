32090739cbc8408b86891d8a09c4e6e6d90ae435
Andrei P <popandrei230@yahoo.com>
2023-10-02 21:07:56 +0300

    Remove scheduled audit job, hello world example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, web, App, HttpServer, Responder, HttpResponse};
+
+#[get("/health_check")]
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok().finish()
+}
+
+#[get("/{name}")]
+async fn hello(name: web::Path<String>) -> impl Responder {
+    format!("Hello {}!", &name)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(health_check).service(hello))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
