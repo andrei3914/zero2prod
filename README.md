e7764d695206906052f1134b49e908e9e8bd9fb9
Andrei P <popandrei230@yahoo.com>
2023-10-05 21:34:10 +0300

    Add configuration types and .yml file

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Email newsletter service written in Rust
 
-Based on Zero to Production: https://www.zero2prod.com/index.html?country=Romania&discount_code=EEU60
+Based on Zero to Production: https://www.zero2prod.com/index.html
--- src/startup.rs
+++ src/startup.rs
@@ -1,6 +1,6 @@
-use std::net::TcpListener;
-use actix_web::{App, HttpServer, web, Result, dev::Server};
 use crate::routes::{health_check, subscribe};
+use actix_web::{dev::Server, web, App, HttpServer, Result};
+use std::net::TcpListener;
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
