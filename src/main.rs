5830645ca677b0b93a1404cedbc4dfaa93de14fb
Andrei Pop <andrei.pop@hpm.ro>
2023-10-07 21:43:15 +0300

    Tests refactoring, create post /subscriptions

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
+use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::{configuration::get_configuration, startup::run};
+use env_logger::Env;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let configuration = get_configuration().expect("Failed to get configuration");
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
 
+    let configuration = get_configuration().expect("Failed to get configuration");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
+
+    let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
 
-    run(listener)?.await
+    run(listener, connection_pool)?.await
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,12 +1,18 @@
 use crate::routes::{health_check, subscribe};
-use actix_web::{dev::Server, web, App, HttpServer, Result};
+use actix_web::{dev::Server, web, web::Data, App, HttpServer, Result};
+use actix_web::middleware::Logger;
+use sqlx::PgPool;
 use std::net::TcpListener;
 
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
+pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
+    let db_pool = Data::new(db_pool);
+
+    let server = HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
+            .app_data(db_pool.clone())
     })
     .listen(listener)?
     .run();
