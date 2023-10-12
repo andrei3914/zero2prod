df0f80971d183e615bdbe62cd54d501bf3fdd1db
Andrei Pop <andrei.pop@hpm.ro>
2023-10-12 21:30:00 +0300

    Add email sending prerequisites

--- src/startup.rs
+++ src/startup.rs
@@ -1,11 +1,17 @@
+use crate::email_client::EmailClient;
 use crate::routes::{health_check, subscribe};
 use actix_web::{dev::Server, web, web::Data, App, HttpServer, Result};
 use sqlx::PgPool;
 use std::net::TcpListener;
 use tracing_actix_web::TracingLogger;
 
-pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
+pub fn run(
+    listener: TcpListener,
+    db_pool: PgPool,
+    email_client: EmailClient,
+) -> Result<Server, std::io::Error> {
     let db_pool = Data::new(db_pool);
+    let email_client = Data::new(email_client);
 
     let server = HttpServer::new(move || {
         App::new()
@@ -13,6 +19,7 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Er
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(db_pool.clone())
+            .app_data(email_client.clone())
     })
     .listen(listener)?
     .run();
