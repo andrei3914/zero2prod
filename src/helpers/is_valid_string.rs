9bca3f78ad991244383d214881fd335438c02283
Andrei P <popandrei230@yahoo.com>
2023-10-18 22:28:56 +0300

    Fix linting issue

--- src/helpers/is_valid_string.rs
+++ src/helpers/is_valid_string.rs
@@ -8,9 +8,5 @@ pub fn is_valid_input_string(s: &str, length: usize) -> bool {
     let forbidden_chars = ['/', '(', ')', '"', '<', '>', '\\', '{', '}'];
     let contains_forbidden_chars = s.chars().any(|g| forbidden_chars.contains(&g));
 
-    if is_empty_or_whitespace || is_too_long || contains_forbidden_chars {
-        return false;
-    } else {
-        return true;
-    }
+    return !(is_empty_or_whitespace || is_too_long || contains_forbidden_chars)
 }
