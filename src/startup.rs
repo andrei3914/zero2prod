c792d80693d12a041529fc794a9af65de2f2f7a5
Andrei P <popandrei230@yahoo.com>
2023-11-01 22:06:13 +0200

    Add home and login routes

--- src/startup.rs
+++ src/startup.rs
@@ -1,6 +1,6 @@
 use crate::configuration::{DatabaseSettings, Settings};
 use crate::email_client::EmailClient;
-use crate::routes::{confirm, health_check, publish_newsletter, subscribe};
+use crate::routes::{confirm, health_check, home, login_form, publish_newsletter, subscribe};
 use actix_web::{dev::Server, web, web::Data, App, HttpServer, Result};
 use sqlx::postgres::PgPoolOptions;
 use sqlx::PgPool;
@@ -74,6 +74,9 @@ pub fn run(
     let server = HttpServer::new(move || {
         App::new()
             .wrap(TracingLogger::default())
+            .route("/", web::get().to(home))
+            .route("/login", web::get().to(login_form))
+            .route("/login", web::post().to(login_form))
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .route("/subscriptions/confirm", web::get().to(confirm))
