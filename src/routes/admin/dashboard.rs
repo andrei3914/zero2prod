6015c498f910a04a44b21e9eac3583766d8e04f5
Andrei P <popandrei230@yahoo.com>
2023-11-07 22:18:10 +0200

    Redirect to login if unauthorized

--- src/routes/admin/dashboard.rs
+++ src/routes/admin/dashboard.rs
@@ -1,19 +1,10 @@
-use actix_web::{
-    http::header::{ContentType, LOCATION},
-    web, HttpResponse,
-};
+use actix_web::{http::header::ContentType, web, HttpResponse};
 use anyhow::Context;
 use sqlx::PgPool;
 use uuid::Uuid;
 
 use crate::session_state::TypedSession;
-
-fn e500<T>(e: T) -> actix_web::Error
-where
-    T: std::fmt::Debug + std::fmt::Display + 'static,
-{
-    actix_web::error::ErrorInternalServerError(e)
-}
+use crate::utils::{e500, see_other};
 
 pub async fn admin_dashboard(
     session: TypedSession,
@@ -22,24 +13,26 @@ pub async fn admin_dashboard(
     let username = if let Some(user_id) = session.get_user_id().map_err(e500)? {
         get_username(user_id, &pool).await.map_err(e500)?
     } else {
-        return Ok(HttpResponse::SeeOther()
-            .insert_header((LOCATION, "/login"))
-            .finish());
+        return Ok(see_other("/login"));
     };
 
     Ok(HttpResponse::Ok()
         .content_type(ContentType::html())
         .body(format!(
             r#"<!DOCTYPE html>
-               <html lang="en">
-               <head>
-               <meta http-equiv="content-type" content="text/html; charset=utf-8">
-               <title>Admin dashboard</title>
-               </head>
-               <body>
-               <p>Welcome {username}!</p>
-               </body>
-               </html>"#
+            <html lang="en">
+            <head>
+            <meta http-equiv="content-type" content="text/html; charset=utf-8">
+            <title>Admin dashboard</title>
+            </head>
+            <body>
+            <p>Welcome {username}!</p>
+            <p>Available actions:</p>
+            <ol>
+            <li><a href="/admin/password">Change password</a></li>
+            </ol>
+            </body>
+            </html>"#,
         )))
 }
 
